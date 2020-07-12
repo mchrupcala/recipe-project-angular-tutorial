895ff8b5dd9db8c1488b6ad20084ac210e1e0bc2
mchrupcala <michael.chrupcala@gmail.com>
2020-07-12 11:52:45 -0400

    Init project. Built component files for app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RecipeListComponent } from './dashboard/recipe/recipe-list/recipe-list.component';
+import { RecipeEditComponent } from './dashboard/recipe/recipe-edit/recipe-edit.component';
+import { GroceryListComponent } from './dashboard/shopping/grocery-list/grocery-list.component';
+import { RecipeItemComponent } from './dashboard/shopping/recipe-item/recipe-item.component';
+import { GroceryEditComponent } from './dashboard/shopping/grocery-edit/grocery-edit.component';
+import { RecipeDetailComponent } from './dashboard/recipe/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    RecipeListComponent,
+    RecipeEditComponent,
+    GroceryListComponent,
+    RecipeItemComponent,
+    GroceryEditComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule
